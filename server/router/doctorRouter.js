0a886714a930591a07399bd847344ea592121046
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-04-05 23:05:43 +0530

    add files

---
+++ server/router/doctorRouter.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import * as doctorController from './../controller/doctorController.js';
+
+const router = express.Router();
+
+router.route('/').get(doctorController.getAllDoctor).post(doctorController.createDoctor);
+
+router
+    .route('/:id')
+    .get(doctorController.getDoctor)
+    .patch(doctorController.updateDoctor)
+    .delete(doctorController.deleteDoctor);
+
+export default router;
