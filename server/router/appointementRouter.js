6c56568d3c3b708e4d0a074d4106ec7aa3532117
Abhay <abhaykumar7007gupta@gmail.com>
2024-04-06 08:17:53 +0530

    files added

--- server/router/appointementRouter.js
+++ server/router/appointementRouter.js
@@ -1,11 +1,24 @@
 import express from "express"
+import { createAppointment, deleteAppointment, getAllAppointment, getAppointement, updateAppointment } from "../controller/appointmentController"
 const router = express.Router()
 
 
 router.get("/freeApointment",freeModel)
-
 router.get("/paidApointment",paidModel)
 
+
+
+router.route("/")
+.post(createAppointment)
+.get(getAllAppointment)
+
+router.use(protectRoute)
+
 router.get("/book",bookAppointment)
+
+router.route("/:id")
+.get(getAppointement)
+.patch(updateAppointment)
+.delete(deleteAppointment)
 
 export default router
