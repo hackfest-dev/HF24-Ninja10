f61d918433aae12aabdc3585cfa478f54b248b66
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-04-06 07:29:05 +0530

    modify files

--- server/controller/doctorController.js
+++ server/controller/doctorController.js
@@ -1,12 +1,6 @@
-
-import doctorModel from "../models/doctorModel"
-import appointmentModel from "../models/appointmentModel"
-
-const getAllDoctor = async function(req,res){
-    try{
-        let doctors = await doctorModel.find()
-        if(!doctors){
-
+import Doctor from '../models/doctorModel';
+import doctorModel from '../models/doctorModel'; // same as doctor
+import appointmentModel from '../models/appointmentModel';
 
 export async function getAllDoctor(req, res) {
     try {
@@ -86,7 +80,7 @@ export async function deleteDoctor(req, res) {
         errorHandler(res, 500, 'internal server error');
     }
 }
-import doctorModel from '../models/doctorModel';
+
 const getAllDoctor = async function (req, res) {
     try {
         let doctors = await doctorModel.find();
@@ -103,58 +97,26 @@ const getAllDoctor = async function (req, res) {
             message: err.message,
         });
     }
-
-}
-
-const getDoctor = async function(req,res){
-    try{
-        let did = req.params.id;
-        let details = await doctorModel.findById(did)
-
-        if(details){
-            res.json({
-                data:details,
-                status:"success"
-            })
-        }
-        else{
-            res.json({
-                status:"fail"
-            })
-        }
-
-    }
-    catch(err){
-        res.json({
-            status:"fail",
-            message:err.message
-        })
-    }
-}
-
-const checkPatientAppointment = async function(req,res){
-    try{
-        let pid = req.params.id
-        let patient = await appointmentModel.find({patient:pid})
-        if(patient){
-            res.json({
-                data:patient,
-                status:"success"
-            })
-        }
-        else{
-            res.json({
-                message:"no appointment exist"
-            })
-        }
-    }
-    catch(err){
-        res.json({
-            status:"fail",
-            message:err.message
-        })
-    }
-}
-
 };
 
+const checkPatientAppointment = async function (req, res) {
+    try {
+        let pid = req.params.id;
+        let patient = await appointmentModel.find({ patient: pid });
+        if (patient) {
+            res.json({
+                data: patient,
+                status: 'success',
+            });
+        } else {
+            res.json({
+                message: 'no appointment exist',
+            });
+        }
+    } catch (err) {
+        res.json({
+            status: 'fail',
+            message: err.message,
+        });
+    }
+};
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,60 +1,59 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import validator from "validator";
-
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import validator from 'validator';
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
     name: {
         type: String,
-        required: [true, "First name is required"],
-        minLength: [3, "First name must contain at least 3 characters"]
+        required: [true, 'First name is required'],
+        minLength: [3, 'First name must contain at least 3 characters'],
     },
     email: {
         type: String,
         unique: true,
-        required: [true, "Email is required"],
-        validate: [validator.isEmail, "Please provide a valid email"],
-        lowercase : true,
+        required: [true, 'Email is required'],
+        validate: [validator.isEmail, 'Please provide a valid email'],
+        lowercase: true,
     },
     phone: {
         type: String,
-        required: [true, "Phone number is required"],
-        minLength: [10, "Phone number must contain exactly 10 digits"],
-        maxLength: [10, "Phone number must contain exactly 10 digits"]
+        required: [true, 'Phone number is required'],
+        minLength: [10, 'Phone number must contain exactly 10 digits'],
+        maxLength: [10, 'Phone number must contain exactly 10 digits'],
     },
     dob: {
         type: Date,
-        required: [true, "Date of birth is required"]
+        required: [true, 'Date of birth is required'],
     },
     gender: {
         type: String,
-        required: [true, "Gender is required"],
-        lowercase : true,
-        enum: ["male", "female", 'others']
+        required: [true, 'Gender is required'],
+        lowercase: true,
+        enum: ['male', 'female', 'others'],
     },
     password: {
         type: String,
-        required: [true, "Password is required"],
-        select: false
+        required: [true, 'Password is required'],
+        select: false,
     },
     role: {
         type: String,
-        required: [true, "Role is required"],
-        default: "patient",
-        enum: ["admin", "patient", "doctor"]
+        required: [true, 'Role is required'],
+        default: 'patient',
+        enum: ['admin', 'patient', 'doctor'],
     },
     doctorDepartment: String,
     docAvatar: {
         public_id: String,
-        url: String
-    }
+        url: String,
+    },
 });
 
-userSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) {
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
         return next();
     }
     try {
@@ -66,7 +65,7 @@ userSchema.pre("save", async function(next) {
     }
 });
 
-userSchema.methods.comparePassword = async function(enteredPassword) {
+userSchema.methods.comparePassword = async function (enteredPassword) {
     try {
         return await bcrypt.compare(enteredPassword, this.password);
     } catch (error) {
@@ -74,12 +73,10 @@ userSchema.methods.comparePassword = async function(enteredPassword) {
     }
 };
 
-userSchema.methods.generateJsonWebToken = function() {
+userSchema.methods.generateJsonWebToken = function () {
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY);
 };
 
-const UserModel = mongoose.model("UserModel", userSchema);
+const UserModel = mongoose.model('UserModel', userSchema);
 
 export default UserModel;
-
-
