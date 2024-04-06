6e9c0d8e20c959e571b61266acfc164ed42d81f7
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-04-06 08:18:35 +0530

    modify app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ import express from 'express';
 import helmet from 'helmet';
 import mongoSantize from 'express-mongo-sanitize';
 
+// importing routes
+import appointmentRouter from './router/appointementRouter.js';
+import doctorRouter from './router/doctorRouter.js';
+import equipmentRouter from './router/equipmentRouter.js';
+import userRouter from './router/userRouter.js';
+
 const app = express();
 
 // set security http header
@@ -12,5 +18,11 @@ app.use(mongoSantize());
 
 // middlewares
 app.use(express.json());
+
+// route setup
+app.use('/api/user', userRouter);
+app.use('/api/equipment', equipmentRouter);
+app.use('/api/doctor', doctorRouter);
+app.use('/api/appointment', appointmentRouter);
 
 export default app;
